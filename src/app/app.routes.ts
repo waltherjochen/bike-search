e22f40f927ea7e7f55a312304d9097287f1e327c
jochenwalther <atlassian@waltherjochen.de>
2024-02-03 11:30:22 +0100

    feat: add search component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/search',
+    pathMatch: 'full'
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./views/search/search.component').then(c => c.SearchComponent)
+  }
+];
